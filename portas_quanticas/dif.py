e01946578d09b5001edc473f12ce8feee3dedc72
CeciliaBotelho <ceciliacbcbcb@gmail.com>
2025-03-18 09:19:03 -0300

    dif arrumado

--- portas_quanticas/dif.py
+++ portas_quanticas/dif.py
@@ -5,4 +5,3 @@ def diff_fuzzy(qc, qubit1, qubit2, aux_qubit):
     """Aplica o operador de diferença fuzzy no circuito."""
     qc.x(qubit2)  # Aplica NOT no segundo qubit
     qc.ccx(qubit1, qubit2, aux_qubit)  # Aplica porta Toffoli
-    qc.x(qubit2)  # Reverte a operação NOT no segundo qubit
